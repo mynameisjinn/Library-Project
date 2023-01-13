6c8e5ce14e6dd84c1feda1938d3a8a53a970e33b
hyejin <obndo8539@naver.com>
2023-01-13 16:15:53 +0900

    Swagger

--- src/main/java/com/korit/library/web/api/AccountApi.java
+++ src/main/java/com/korit/library/web/api/AccountApi.java
@@ -4,6 +4,7 @@ import com.korit.library.aop.annotation.ValidAspect;
 import com.korit.library.service.AccountService;
 import com.korit.library.web.dto.CMRespDto;
 import com.korit.library.web.dto.UserDto;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.net.URI;
 
+@Api(tags = {"Account Rest API Controller"} )
 @RestController
 @RequestMapping("/api/account")
 public class AccountApi {
@@ -20,9 +22,10 @@ public class AccountApi {
     @Autowired
     private AccountService accountService;
 
+    @ApiOperation(value = "회원가입", notes = "회원가입 요청 메소드")
     @ValidAspect
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
+    public ResponseEntity<? extends CMRespDto<? extends UserDto>> register(@RequestBody @Valid UserDto userDto, BindingResult bindingResult) {
 
         accountService.duplicateUsername(userDto.getUsername());
         accountService.compareToPassword(userDto.getPassword(), userDto.getRepassword());
@@ -34,8 +37,19 @@ public class AccountApi {
                 .body(new CMRespDto<>(HttpStatus.CREATED.value(),"Create a new User", user));
     }
 
+    @ApiOperation(value = "회원가입", notes = "회원가입 조회 메소드")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="userId", value="사용자 식별 코드",required=true, dataType = "int")
+    })
+    @ApiResponses({
+        @ApiResponse(code=400, message="클라이언트가 잘못했음"),
+        @ApiResponse(code=401, message="클라이언트가 잘못했음2")
+    })
     @GetMapping("/user/{userId}")
-    public ResponseEntity<?> getUser(@PathVariable int userId) {
+    public ResponseEntity<? extends CMRespDto<? extends UserDto>> getUser(
+//            @ApiParam(value = "사용자 식별 코드")
+            @PathVariable int userId) {
+
         return ResponseEntity
                 .ok()
                 .body(new CMRespDto<>(HttpStatus.OK.value(),"Success", accountService.getUser(userId)));
--- src/main/java/com/korit/library/web/dto/UserDto.java
+++ src/main/java/com/korit/library/web/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.korit.library.web.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,19 +19,24 @@ public class UserDto {
     private int userId;
 
     @NotBlank
+    @ApiModelProperty(name="username", value="사용자이름", example = "abc", required = true)
     private String username;
 
     @NotBlank
+    @ApiModelProperty(name="password", value="비밀번호", example = "1234", required = true)
     private String password;
 
     @NotBlank
+    @ApiModelProperty(name="repassword", value="비밀번호 확인", example = "1234", required = true)
     private String repassword;
 
     @NotBlank
+    @ApiModelProperty(name="name", value="성명", example = "김ab", required = true)
     private String name;
 
     @NotBlank
     @Email
+    @ApiModelProperty(name="email", value="이메일", example = "abc@gmail.com", required = true)
     private String email;
 
     private List<RoleDtlDto> roleDtlDto;
