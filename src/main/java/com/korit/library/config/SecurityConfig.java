1fda31a57a572553f9eec2b984a79f343418da29
hyejin <obndo8539@naver.com>
2023-01-26 16:16:34 +0900

    회원가입 성공시, 로그인 실패시

--- src/main/java/com/korit/library/config/SecurityConfig.java
+++ src/main/java/com/korit/library/config/SecurityConfig.java
@@ -31,12 +31,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/mypage/**", "/security/**")
                 .authenticated()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")   // ROLE_ADMIN, ROLE_MANAGER
                 .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/account/login") // 로그인 페이지 get요청
                 .loginProcessingUrl("/account/login") // 로그인 인증 post 요청
+                .failureForwardUrl("/account/login/error")
                 .defaultSuccessUrl("/index");
     }
 }
--- src/main/java/com/korit/library/security/PrincipalDetails.java
+++ src/main/java/com/korit/library/security/PrincipalDetails.java
@@ -1,5 +1,7 @@
 package com.korit.library.security;
 
+import com.korit.library.web.dto.RoleDtlDto;
+import com.korit.library.web.dto.RoleMstDto;
 import com.korit.library.web.dto.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -20,13 +23,29 @@ public class PrincipalDetails implements UserDetails {
     private Map<String, Object> response;
 
 
+    // 권한을 리스트로 관리하는 부분
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         ArrayList<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
 
-        user.getRoleDtlDto().forEach(dtl -> {
-            authorities.add(() -> dtl.getRoleMstDto().getRoleName());
-        });
+        List<RoleDtlDto> roleDtlDtoList = user.getRoleDtlDto();
+        for(int i = 0; i < roleDtlDtoList.size(); i++) {
+            RoleDtlDto dtl = roleDtlDtoList.get(i); // 0 = ROLE_USER, 1 = ROLE_ADMIN
+            RoleMstDto roleMstDto = dtl.getRoleMstDto();
+            String roleName = roleMstDto.getRoleName();
+
+            GrantedAuthority role = new GrantedAuthority() {
+                @Override
+                public String getAuthority() {
+                    return roleName;
+                }
+            };
+            authorities.add(role);
+        }
+
+//        user.getRoleDtlDto().forEach(dtl -> {
+//            authorities.add(() -> dtl.getRoleMstDto().getRoleName());
+//        });
 
         return authorities;
     }
@@ -41,21 +60,33 @@ public class PrincipalDetails implements UserDetails {
         return user.getUsername();
     }
 
+    /*
+        계정 만료 여부
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
-
+    /*
+        계정 잠김 여부 : 휴면계정
+    */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
-
+    /*
+        비밀번호 만료 여부 :
+        일정횟수이상 비밀번호 틀렸을 경우
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
-
+    /*
+        사용자 활성화 여부 :
+        회원가입 시 본인 인증을 한경우 or 휴면계정이 풀린 경우 -> true
+        인증이 되지않은 경우 -> false
+     */
     @Override
     public boolean isEnabled() {
         return true;
