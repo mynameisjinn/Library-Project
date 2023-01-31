396c4593b133955712b95618b3043b6f84a10a43
hyejin <obndo8539@naver.com>
2023-01-31 23:07:36 +0900

    도서목록 페이지 번호 불러오기 중 (오류있음)

--- src/main/java/com/korit/library/repository/BookRepository.java
+++ src/main/java/com/korit/library/repository/BookRepository.java
@@ -27,7 +27,7 @@ public interface BookRepository {
     U:  도서수정
     D:  도서삭제
      */
-
+     public int getBookTotalCount(SearchNumberListReqDto searchNumberListReqDto);
      public List<BookMst> searchBook(SearchReqDto searchReqDto);
      public BookMst findBookByBookCode(String bookCode);
      public List<CategoryView> findAllCategory();
--- src/main/resources/static/js/admin/book_search.js
+++ src/main/resources/static/js/admin/book_search.js
@@ -5,7 +5,7 @@ window.onload = () => {
 }
 
 let searchObj = {
-    page : 1,
+    page : 5,
     category : "",
     searchValue : "",
     order : "bookId",
@@ -39,28 +39,54 @@ class BookSearchApi {
                 console.log(error);
             }
         });
+
         return returnData;
     }
 
-   getCategories(){
+    getBookTotalCount(searchObj) {
+        let returnData = null;
+
+        $.ajax({
+            async: false,
+            type: "get",
+            url: "http://127.0.0.1:8000/api/admin/books/totalcount",
+            data: {
+                "category" : searchObj.category,
+                "searchValue" : searchObj.searchValue
+            },
+            dataType: "json",
+            success: response => {
+                console.log(response);
+                returnData = response.data;
+            },
+            error: error => {
+                console.log(error);
+            }
+        });
+
+        return returnData;
+    }
+
+    getCategories() {
         let returnData = null;
 
         $.ajax({
             async: false,
             type: "get",
             url: "http://127.0.0.1:8000/api/admin/categories",
-            data: searchObj,
             dataType: "json",
             success: response => {
                 console.log(response);
                 returnData = response.data;
-             },
+            },
             error: error => {
                 console.log(error);
             }
         });
+
         return returnData;
-   }
+    }
+
 }
 
 class BookService {
@@ -91,9 +117,70 @@ class BookService {
                     <td>${data.category}</td>
                     <td>${data.rentalStatus == "Y" ? "대여중" : "대여가능"}</td>
                     <td><i class="fa-solid fa-square-pen"></i></td>
-                    <td><i class="fa-solid fa-square-minus"></i></td>
                 </tr>
             `;
+        });
+
+        this.loadSearchNumberList();
+    }
+
+    loadSearchNumberList() {
+        const pageController = document.querySelector(".page-controller");
+
+        const totalCount = BookSearchApi.getInstance().getBookTotalCount(searchObj);
+        const maxPageNumber = totalCount % searchObj.count == 0
+                            ? Math.floor(totalCount / searchObj.count)
+                            : Math.floor(totalCount / searchObj.count) + 1;
+
+        pageController.innerHTML = `
+            <a href="javascript:void(0)" class="pre-button disabled">이전</a>
+            <ul class="page-numbers">
+            </ul>
+            <a href="javascript:void(0)" class="next-button disabled">다음</a>
+        `;
+
+        if(searchObj.page != 1) {
+            const preButton = pageController.querySelector(".pre-button");
+            preButton.classList.remove("disabled");
+
+            preButton.onclick = () => {
+                searchObj.page--;
+                this.loadBookList();
+            }
+        }
+
+        if(searchObj.page != maxPageNumber) {
+            const nextButton = pageController.querySelector(".next-button");
+            nextButton.classList.remove("disabled");
+
+            nextButton.onclick = () => {
+                searchObj.page++;
+                this.loadBookList();
+            }
+        }
+
+        const startIndex = searchObj.page % 5 == 0
+                        ? searchObj.page - 4
+                        : searchObj.page - (searchObj.page % 5) + 1;
+        const endIndex = startIndex + 4 <= maxPageNumber ? startIndex + 4 : maxPageNumber;
+        const pageNumbers = document.querySelector(".page-numbers");
+
+        for(let i = startIndex; i <= endIndex; i++) {
+            pageNumbers.innerHTML += `
+                <a href="javascript:void(0)"class="page-button ${i == searchObj.page ? "disabled" : ""}"><li>${i}</li></a>
+            `;
+        }
+
+        const pageButtons = document.querySelectorAll(".page-button");
+        pageButtons.forEach(button => {
+
+            const pageNumber = button.textContent;
+            if(pageNumber != searchObj.page) {
+                button.onclick = () => {
+                    searchObj.page = pageNumber;
+                    this.loadBookList();
+                }
+            }
         });
     }
 
@@ -101,43 +188,41 @@ class BookService {
         const responseData = BookSearchApi.getInstance().getCategories();
 
         const categorySelect = document.querySelector(".category-select");
-        categorySelect.innerHTML = `<option  value="">전체조회</option>`;
+        categorySelect.innerHTML = `<option value="">전체조회</option>`;
 
-        responseData.forEach((data => {
+        responseData.forEach(data => {
             categorySelect.innerHTML += `
                 <option value="${data.category}">${data.category}</option>
             `;
-        }));
-
+        });
     }
 }
 
 class ComponentEvent {
     static #instance = null;
-        static getInstance() {
-            if(this.#instance == null) {
-                this.#instance = new ComponentEvent();
-            }
-            return this.#instance;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new ComponentEvent();
+        }
+        return this.#instance;
+    }
+
+    addClickEventSearchButton() {
+        const categorySelect = document.querySelector(".category-select");
+        const searchInput = document.querySelector(".search-input");
+        const searchButton = document.querySelector(".search-button");
+
+        searchButton.onclick = () => {
+            searchObj.category = categorySelect.value;
+            searchObj.searchValue = searchInput.value;
+            searchObj.page = 1;
+            BookService.getInstance().loadBookList();
         }
 
-        addClickEventSearchButton() {
-            const categorySelect = document.querySelector(".category-select");
-            const searchInput = document.querySelector(".search-input");
-            const searchButton = document.querySelector(".search-button");
-
-            searchButton.onclick = () => {
-                searchObj.category = categorySelect.value;
-                searchObj.searchValue = searchInput.value;
-
-                BookService.getInstance().loadBookList();
+        searchInput.onkeyup = () => {
+            if(window.event.keyCode == 13) {
+                searchButton.click();
             }
-
-            searchInput.onkeyup = () => {
-                if(window.event.keyCode == 13) {
-                    searchButton.click();
-                }
-            }
-
         }
+    }
 }
